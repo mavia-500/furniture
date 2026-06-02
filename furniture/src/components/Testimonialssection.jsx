b2f2d4f3d2e3100b9513a5789f882835229193b4
mavia-500 <maviasajjadabbasi@gmail.com>
2026-06-02 18:41:18 +0500

    add customers numbers

--- furniture/src/components/Testimonialssection.jsx
+++ furniture/src/components/Testimonialssection.jsx
@@ -512,7 +512,7 @@ export default function TestimonialsSection() {
             className="flex flex-col gap-1.5"
           >
             <span className="font-serif text-3xl font-light text-[#C8A96E] leading-none">
-              {s.value}
+              5000
             </span>
 
             <span className="font-sans text-[11px] font-medium tracking-[2px] uppercase text-[#9A8A70]">
